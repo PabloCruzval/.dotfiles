c80c4382f60feea33a134803736082547701de58
PabloCruzval <p.cruzat.valenzuela@gmail.com>
2024-11-16 12:55:59 -0300

    Hyprland theme script change

--- hypr/.config/hypr/scripts/change.py
+++ hypr/.config/hypr/scripts/change.py
@@ -10,14 +10,16 @@ config.read(config_files)
 
 print("Configuraciones disponibles")
 for seccion in config.sections():
-    print(f"{seccion}")
+    print(f"{config.sections().index(seccion)+1}: {seccion}")
 
 
 while True:
-    option = input("Escribe el nombre de la configuracion: ")
-    if option in config.sections():
+    option = input("Escribe el numero de la configuracion: ")
+    option = int(option) - 1
+    if option < len(config.sections()) and option >= 0:
+        config_name = config.sections()[option]
         break
-    print("Nombre invalido")
+    print("Indice invalido")
 
 # Obtain each line from the hyprland files
 with open(hypr_visual, 'r') as file:
@@ -30,10 +32,10 @@ with open(hypr_visual, 'w') as file:
     borders = [0, 0]
     for line in visual_lines:
         if "$active_border_one" in line and not borders[0]:
-            file.write(f"$active_border_one = {config[option]["active_border_one"]}\n")
+            file.write(f"$active_border_one = {config[config_name]["active_border_one"]}\n")
             borders[0] = 1
         elif "$active_border_two" in line and not borders[1]:
-            file.write(f"$active_border_two = {config[option]["active_border_two"]}\n")
+            file.write(f"$active_border_two = {config[config_name]["active_border_two"]}\n")
             borders[1] = 1
         else:
             file.write(line)
@@ -43,15 +45,15 @@ with open(hypr_paper, 'w') as file:
     wallpapers = [0, 0]
     for line in paper_lines:
         if "$wallpaper_one" in line and not wallpapers[0]:
-            file.write(f"$wallpaper_one = {config[option]["wallpaper_one"]}\n")
+            file.write(f"$wallpaper_one = {config[config_name]["wallpaper_one"]}\n")
             wallpapers[0] = 1
         elif "$wallpaper_two" in line and not wallpapers[1]:
-            file.write(f"$wallpaper_two = {config[option]["wallpaper_two"]}\n")
+            file.write(f"$wallpaper_two = {config[config_name]["wallpaper_two"]}\n")
             wallpapers[1] = 1
         else:
             file.write(line)
 
 
-os.system("hyprctl reload")
-os.system(f"hyprctl hyprpaper wallpaper \"DP-1,{config[option]["wallpaper_one_dir"]}\"")
-os.system(f"hyprctl hyprpaper wallpaper \"HDMI-A-1,{config[option]["wallpaper_two_dir"]}\"")
+os.system("hyprctl reload > nul")
+os.system(f"hyprctl hyprpaper wallpaper \"DP-1,{config[config_name]["wallpaper_one_dir"]}\" > nul")
+os.system(f"hyprctl hyprpaper wallpaper \"HDMI-A-1,{config[config_name]["wallpaper_two_dir"]}\" > nul")
